73271cf714a6bd57661310a2a4be65b01a692be4
Dalton Spillman <daltonks@users.noreply.github.com>
2020-10-25 21:43:24 -0500

    Initial code commit

---
+++ RichTextKitIssue24/RichTextKitIssue24/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Topten.RichTextKit;
+using Xamarin.Forms;
+
+namespace RichTextKitIssue24
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var canvas = e.Surface.Canvas;
+
+            canvas.Clear(SKColors.White);
+
+            var richString = new RichString()
+                .Add("Big text", fontSize: 40)
+                .Add("Little text", fontSize: 12);
+            richString.Paint(canvas, new SKPoint(0, 100));
+        }
+    }
+}
